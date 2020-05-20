68e8d1d1b1e5daa0180ce1ad0d06861f762b2fc8
germa1833379 <germa1833379@etu.cegepjonquiere.ca>
2020-05-20 15:34:02 -0400

    Force 3d fonctionne , + angleX + graphiqueX

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ import java.util.Vector;
 
 public class Main extends Application {
     ArrayList<XY> trajectoire = new ArrayList<>();
+    ArrayList<XY> trajectoire2 = new ArrayList<>();
     public static float hitHeight;
     public static void main(String[] args) {
         launch(args);
@@ -55,16 +56,35 @@ public class Main extends Application {
         primaryStage.setMaximized(true);
         Gun currGun = new Gun();
         Scenery.getShoot().setOnAction(event -> {
-            XYChart.Series trajectoireChart=getChart(2000,Bullet.getG1Bullet(),currGun);
+
+            //Chart Trajectoire
+            XYChart.Series trajectoireChart=getHorizontalChart(2000,Bullet.getG1Bullet(),currGun);
             Target currTarget=new Target(0.5f,1,scenery.getPositionCible()*50+100);
+            XYChart.Series cibleSeries= new XYChart.Series();
+            cibleSeries.setName("Cible");
+            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getHeight()+currTarget.getWidht()));
+            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getHeight()-currTarget.getWidht()));
             Scenery.chart.getData().clear();
             Scenery.chart.getData().addAll(trajectoireChart);
+            Scenery.chart.getData().addAll(cibleSeries);
+            //Chart BirdEye
+            XYChart.Series birdEyeChart=getBirdEyesChart(2000,Bullet.getG1Bullet(),currGun);
+            XYChart.Series cibleSeries2= new XYChart.Series();cibleSeries2.setName("Cible");
+            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getWidht()));
+            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(),-currTarget.getWidht()));
+            Scenery.chart2.getData().clear();
+            Scenery.chart2.getData().addAll(birdEyeChart);
+            Scenery.chart2.getData().addAll(cibleSeries2);
+
             for(int i=0;i<trajectoireChart.getData().size()-1;i++){
                 currTarget.isHit(
                         trajectoire.get(i+1).getX(),
                         trajectoire.get(i+1).getY(),
                         trajectoire.get(i).getX(),
-                        trajectoire.get(i).getY());
+                        trajectoire.get(i).getY(),
+                        trajectoire2.get(i+1).getY(),
+                        trajectoire2.get(i).getY()
+                        );
             }
         });
 
@@ -78,7 +98,8 @@ public class Main extends Application {
         });
 
     }
-    public XYChart.Series getChart(int distanceMax,Bullet currentBullet,Gun currentGun){
+    int maxX;
+    public XYChart.Series getHorizontalChart(int distanceMax,Bullet currentBullet,Gun currentGun){
         XYChart.Series series = new XYChart.Series();
         series.setName("Trajectory");
 
@@ -95,12 +116,13 @@ public class Main extends Application {
         float totDistTraveled=1; // REFERING TO THE DISTANCE TRAVELLED IN THE AIR STARTS AT 1 BECAUSE OF FOR COMPOSITION
         for(int i=0;i<distanceMax;i++){
 
-            double tpmx=(1.0/(currentBullet.getCurrSpeed(totDistTraveled)*Math.cos(Math.toRadians(currentGun.getAngleY())))); //TIME PER METRE X
-             timeElapsed += tpmx;
+            double tpmx=(1.0/(currentBullet.getCurrSpeed(totDistTraveled)*Math.cos(Math.toRadians(currentGun.getAngleY())*Math.cos(Math.toRadians(currentGun.getAngleX()))))); //TIME PER METRE X
+            timeElapsed += tpmx;
             //Force en descendant
             float Yforce=-9.8f;
             double variationY=currYSpeed*timeElapsed+(0.5*Yforce*timeElapsed*timeElapsed);
             if (variationY+startHeight<0){
+                maxX=i+1;
                 break;
             }
             series.getData().add(new XYChart.Data(i+1,variationY+startHeight));
@@ -109,7 +131,37 @@ public class Main extends Application {
             lastHeight+=variationY;
         }
         currentBullet.reset();
-        System.out.println(currYSpeed);
+        return series;
+    }
+    public XYChart.Series getBirdEyesChart(int distanceMax,Bullet currentBullet,Gun currentGun){
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Bird Eye View");
+
+
+        float startHeight=0;
+        float lastHeight=startHeight;
+        float currYSpeed=(float)(Math.sin(Math.toRadians(currentGun.getAngleX()))*currentBullet.getStartSpeed());
+
+        series.getData().add(new XYChart.Data(0,startHeight));
+        trajectoire2.clear();
+        trajectoire2.add(new XY(0,startHeight));
+
+        double timeElapsed = 0;
+        for(int i=0;i<distanceMax;i++){
+            if(i>=maxX)
+                break;
+            double tpmx=(1.0/(currentBullet.getStartSpeed()*Math.cos(Math.toRadians(currentGun.getAngleY()))*Math.cos(Math.toRadians(currentGun.getAngleX())))); //TIME PER METRE X
+            timeElapsed += tpmx;
+            //Force en descendant
+            float Yforce=0f;
+            double variationY=currYSpeed*timeElapsed+(0.5*Yforce*timeElapsed*timeElapsed);
+            series.getData().add(new XYChart.Data(i+1,variationY+startHeight));
+            trajectoire2.add(new XY((double)i+1.0,variationY+startHeight));
+
+            lastHeight+=variationY;
+
+        }
+        currentBullet.reset();
         return series;
     }
 
