5a078537112722c544195dec24a36d2ec1f57366
gobto1833006 <gobto1833006@etu.cegepjonquiere.ca>
2020-05-21 15:08:21 -0400

    Yo kim

--- src/Main.java
+++ src/Main.java
@@ -10,46 +10,54 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.Vector;
 
 public class Main extends Application {
     ArrayList<XY> trajectoire = new ArrayList<>();
     ArrayList<XY> trajectoire2 = new ArrayList<>();
     public static float hitHeight;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        Wind wind = new Wind(0, 0);
         //Scene
-        Camera camera=new PerspectiveCamera();
+        Camera camera = new PerspectiveCamera();
         camera.setNearClip(0);
-        double sceneWidth=1285;
+        double sceneWidth = 1285;
 
-        Scenery scenery=new Scenery();
-        Group root=scenery.getScene();
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run(){
+                fixingView(camera);
+            }
+        },1,1);
+        Scenery scenery = new Scenery();
+        Group root = scenery.getScene();
         Scene scene = new Scene(root);
 
         scene.setFill(Color.SKYBLUE);
-        root.setOnMouseClicked(event -> {
-            camera.setTranslateZ(camera.getTranslateZ()+0.01);
-            camera.setTranslateZ(camera.getTranslateZ()-0.01);
-        });
+
 
         scene.setCamera(camera);
 
         scene.widthProperty().addListener((observable, oldValue, newValue) -> {
             root.setTranslateX(scene.getWidth());
-            camera.setTranslateX(1922.5+(scene.getWidth()-1285)/2);
+            camera.setTranslateX(1922.5 + (scene.getWidth() - 1285) / 2);
         });
 
         scene.heightProperty().addListener((observable, oldValue, newValue) -> {
             root.setTranslateY(scene.getHeight());
             root.setTranslateZ(scene.getHeight());
-            camera.setTranslateZ(-36+(2.865*(scene.getHeight()-721)));
-            camera.setTranslateY(1079.5+(scene.getHeight()-721)/2);
+            camera.setTranslateZ(-36 + (2.865 * (scene.getHeight() - 721)));
+            camera.setTranslateY(1079.5 + (scene.getHeight() - 721) / 2);
         });
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -58,36 +66,43 @@ public class Main extends Application {
         Scenery.getShoot().setOnAction(event -> {
 
             //Chart Trajectoire
-            XYChart.Series trajectoireChart=getHorizontalChart(2000,Bullet.getG1Bullet(),currGun);
-            Target currTarget=new Target(0.5f,1,scenery.getPositionCible()*50+100);
-            XYChart.Series cibleSeries= new XYChart.Series();
+            XYChart.Series trajectoireChart = getHorizontalChart(2000, Bullet.getG1Bullet(), currGun);
+            Target currTarget = new Target(0.5f, 1, scenery.getPositionCible() * 100 + 100);
+            XYChart.Series cibleSeries = new XYChart.Series();
             cibleSeries.setName("Cible");
-            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getHeight()+currTarget.getWidht()));
-            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getHeight()-currTarget.getWidht()));
+            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(), currTarget.getHeight() + currTarget.getWidht()));
+            cibleSeries.getData().add(new XYChart.Data(currTarget.getTargetX(), currTarget.getHeight() - currTarget.getWidht()));
             Scenery.chart.getData().clear();
             Scenery.chart.getData().addAll(trajectoireChart);
             Scenery.chart.getData().addAll(cibleSeries);
             //Chart BirdEye
-            XYChart.Series birdEyeChart=getBirdEyesChart(2000,Bullet.getG1Bullet(),currGun);
-            XYChart.Series cibleSeries2= new XYChart.Series();cibleSeries2.setName("Cible");
-            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(),currTarget.getWidht()));
-            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(),-currTarget.getWidht()));
+            XYChart.Series birdEyeChart = getBirdEyesChart(2000, Bullet.getG1Bullet(), currGun);
+            XYChart.Series cibleSeries2 = new XYChart.Series();
+            cibleSeries2.setName("Cible");
+            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(), currTarget.getWidht()));
+            cibleSeries2.getData().add(new XYChart.Data(currTarget.getTargetX(), -currTarget.getWidht()));
             Scenery.chart2.getData().clear();
             Scenery.chart2.getData().addAll(birdEyeChart);
             Scenery.chart2.getData().addAll(cibleSeries2);
 
-            for(int i=0;i<trajectoireChart.getData().size()-1;i++){
+            for (int i = 0; i < trajectoireChart.getData().size() - 1; i++) {
+
+
                 currTarget.isHit(
-                        trajectoire.get(i+1).getX(),
-                        trajectoire.get(i+1).getY(),
+                        trajectoire.get(i + 1).getX(),
+                        trajectoire.get(i + 1).getY(),
                         trajectoire.get(i).getX(),
                         trajectoire.get(i).getY(),
-                        trajectoire2.get(i+1).getY(),
+                        trajectoire2.get(i + 1).getY(),
                         trajectoire2.get(i).getY()
-                        );
-            }
-        });
 
+                );
+                fixingView(camera);
+            }
+
+
+
+        });
 
 
         Scenery.getAngleXSlider().valueProperty().addListener((observable, oldValue, newValue) -> {
@@ -96,73 +111,87 @@ public class Main extends Application {
         Scenery.getAngleYSlider().valueProperty().addListener((observable, oldValue, newValue) -> {
             currGun.setAngleY(newValue.floatValue());
         });
+        Scenery.getVentX().valueProperty().addListener((observable, oldValue, newValue) -> {
+            wind.setForceX(newValue.intValue());
+        });
+        Scenery.getVentZ().valueProperty().addListener((observable, oldValue, newValue) -> {
+            wind.setForceZ(newValue.intValue());
+        });
 
     }
+
     int maxX;
-    public XYChart.Series getHorizontalChart(int distanceMax,Bullet currentBullet,Gun currentGun){
+
+    public XYChart.Series getHorizontalChart(int distanceMax, Bullet currentBullet, Gun currentGun) {
         XYChart.Series series = new XYChart.Series();
         series.setName("Trajectory");
 
 
-        float startHeight=1;
-        float lastHeight=startHeight;
-        float currYSpeed=(float)(Math.sin(Math.toRadians(currentGun.getAngleY()))*currentBullet.getStartSpeed());
+        float startHeight = 1;
+        float lastHeight = startHeight;
+        float currYSpeed = (float) (Math.sin(Math.toRadians(currentGun.getAngleY())) * currentBullet.getStartSpeed());
 
-        series.getData().add(new XYChart.Data(0,startHeight));
+        series.getData().add(new XYChart.Data(0, startHeight));
         trajectoire.clear();
-        trajectoire.add(new XY(0,startHeight));
+        trajectoire.add(new XY(0, startHeight));
 
         double timeElapsed = 0;
-        float totDistTraveled=1; // REFERING TO THE DISTANCE TRAVELLED IN THE AIR STARTS AT 1 BECAUSE OF FOR COMPOSITION
-        for(int i=0;i<distanceMax;i++){
+        float totDistTraveled = 1; // REFERING TO THE DISTANCE TRAVELLED IN THE AIR STARTS AT 1 BECAUSE OF FOR COMPOSITION
+        for (int i = 0; i < distanceMax; i++) {
 
-            double tpmx=(1.0/(currentBullet.getCurrSpeed(totDistTraveled)*Math.cos(Math.toRadians(currentGun.getAngleY())*Math.cos(Math.toRadians(currentGun.getAngleX()))))); //TIME PER METRE X
+            double tpmx = (1.0 / (currentBullet.getCurrSpeed(totDistTraveled) * Math.cos(Math.toRadians(currentGun.getAngleY()) * Math.cos(Math.toRadians(currentGun.getAngleX()))))); //TIME PER METRE X
             timeElapsed += tpmx;
             //Force en descendant
-            float Yforce=-9.8f;
-            double variationY=currYSpeed*timeElapsed+(0.5*Yforce*timeElapsed*timeElapsed);
-            if (variationY+startHeight<0){
-                maxX=i+1;
+            float Yforce = -9.8f;
+            double variationY = currYSpeed * timeElapsed + (0.5 * Yforce * timeElapsed * timeElapsed);
+            if (variationY + startHeight < 0) {
+                maxX = i + 1;
                 break;
             }
-            series.getData().add(new XYChart.Data(i+1,variationY+startHeight));
-            trajectoire.add(new XY((double)i+1.0,variationY+startHeight));
-            totDistTraveled+=1+Math.abs(variationY);
-            lastHeight+=variationY;
+            series.getData().add(new XYChart.Data(i + 1, variationY + startHeight));
+            trajectoire.add(new XY((double) i + 1.0, variationY + startHeight));
+            totDistTraveled += 1 + Math.abs(variationY);
+            lastHeight += variationY;
         }
         currentBullet.reset();
         return series;
     }
-    public XYChart.Series getBirdEyesChart(int distanceMax,Bullet currentBullet,Gun currentGun){
+
+    public XYChart.Series getBirdEyesChart(int distanceMax, Bullet currentBullet, Gun currentGun) {
         XYChart.Series series = new XYChart.Series();
         series.setName("Bird Eye View");
 
 
-        float startHeight=0;
-        float lastHeight=startHeight;
-        float currYSpeed=(float)(Math.sin(Math.toRadians(currentGun.getAngleX()))*currentBullet.getStartSpeed());
+        float startHeight = 0;
+        float lastHeight = startHeight;
+        float currYSpeed = (float) (Math.sin(Math.toRadians(currentGun.getAngleX())) * currentBullet.getStartSpeed());
 
-        series.getData().add(new XYChart.Data(0,startHeight));
+        series.getData().add(new XYChart.Data(0, startHeight));
         trajectoire2.clear();
-        trajectoire2.add(new XY(0,startHeight));
+        trajectoire2.add(new XY(0, startHeight));
 
         double timeElapsed = 0;
-        for(int i=0;i<distanceMax;i++){
-            if(i>=maxX)
+        for (int i = 0; i < distanceMax; i++) {
+            if (i >= maxX)
                 break;
-            double tpmx=(1.0/(currentBullet.getStartSpeed()*Math.cos(Math.toRadians(currentGun.getAngleY()))*Math.cos(Math.toRadians(currentGun.getAngleX())))); //TIME PER METRE X
+            double tpmx = (1.0 / (currentBullet.getStartSpeed() * Math.cos(Math.toRadians(currentGun.getAngleY())) * Math.cos(Math.toRadians(currentGun.getAngleX())))); //TIME PER METRE X
             timeElapsed += tpmx;
             //Force en descendant
-            float Yforce=0f;
-            double variationY=currYSpeed*timeElapsed+(0.5*Yforce*timeElapsed*timeElapsed);
-            series.getData().add(new XYChart.Data(i+1,variationY+startHeight));
-            trajectoire2.add(new XY((double)i+1.0,variationY+startHeight));
+            float Yforce = 0f;
+            double variationY = currYSpeed * timeElapsed + (0.5 * Yforce * timeElapsed * timeElapsed);
+            series.getData().add(new XYChart.Data(i + 1, variationY + startHeight));
+            trajectoire2.add(new XY((double) i + 1.0, variationY + startHeight));
 
-            lastHeight+=variationY;
+            lastHeight += variationY;
 
         }
         currentBullet.reset();
         return series;
+    }
+
+    public void fixingView(Camera camera) {
+        camera.setTranslateZ(camera.getTranslateZ() + 0.01);
+        camera.setTranslateZ(camera.getTranslateZ() - 0.01);
     }
 
 }
--- src/Scenery.java
+++ src/Scenery.java
@@ -17,13 +17,15 @@ import javafx.scene.transform.Rotate;
 
 public class Scenery {
 
-    public static LineChart<Number,Number> chart;
-    public static LineChart<Number,Number> chart2;
+    public static LineChart<Number, Number> chart;
+    public static LineChart<Number, Number> chart2;
     private Group root;
     private static Slider angleX = new Slider(-3, 3, 0);
     private static Slider angleY = new Slider(0, 0.65, 0);
     private static Button shoot = new Button("Fire in the hole");
-    private int positionCible=0;
+    private static Slider ventX = new Slider(-100, 100, 0);
+    private static Slider ventZ = new Slider(-100, 100, 0);
+    private int positionCible = 0;
 
 
     public Scenery() {
@@ -91,21 +93,22 @@ public class Scenery {
                 boutons[i].setTranslateY(715.3 + 0.25 * i);
                 boutons[i].setTranslateZ(-2095);
                 boutons[i].setOnMouseClicked(event -> {
-                    cible.setTranslateZ(-2074 + 50 * Integer.parseInt(label.getText()));
-                    positionCible=Integer.parseInt(label.getText());
+                    cible.setTranslateZ(-2074 + 100 * Integer.parseInt(label.getText()));
+                    positionCible = Integer.parseInt(label.getText());
                 });
                 root.getChildren().add(boutons[i]);
             }
-            for (int i=0;i<boutons.length;i++){
-            Label labeldistance =new Label(i*50 +100 +" mètres");
-            labeldistance.setTextFill(Color.WHITE);
-            labeldistance.setTranslateX(390);
-            labeldistance.setTranslateY(210+94*i);
-            //210 à 870 environ
-            labeldistance.setScaleX(1.5);
-            labeldistance.setScaleY(1.5);
-            labeldistance.mouseTransparentProperty().setValue(true);
-            root.getChildren().add(labeldistance);}
+            for (int i = 0; i < boutons.length; i++) {
+                Label labeldistance = new Label(i * 100 + 100 + " mètres");
+                labeldistance.setTextFill(Color.WHITE);
+                labeldistance.setTranslateX(390);
+                labeldistance.setTranslateY(210 + 94 * i);
+                //210 à 870 environ
+                labeldistance.setScaleX(1.5);
+                labeldistance.setScaleY(1.5);
+                labeldistance.mouseTransparentProperty().setValue(true);
+                root.getChildren().add(labeldistance);
+            }
 
             Sphere cylinder = new Sphere(0.01);
             Material material2 = new PhongMaterial();
@@ -179,8 +182,6 @@ public class Scenery {
             root.getChildren().add(new AmbientLight(Color.WHITE));*/
 
 
-
-
             angleX.setTranslateX(1300);
             angleX.setTranslateY(1200);
             angleX.setBlockIncrement(0.2);
@@ -196,18 +197,33 @@ public class Scenery {
             angleY.setMajorTickUnit(0.13);
             angleY.setShowTickLabels(true);
 
+            ventX.setTranslateX(500);
+            ventX.setTranslateY(1200);
+            ventX.setBlockIncrement(1);
+            ventX.setShowTickMarks(true);
+            ventX.setMajorTickUnit(20);
+            ventX.setShowTickLabels(true);
+
+            ventZ.setTranslateX(400);
+            ventZ.setTranslateY(1100);
+            ventZ.setOrientation(Orientation.VERTICAL);
+            ventZ.setBlockIncrement(1);
+            ventZ.setShowTickMarks(true);
+            ventZ.setMajorTickUnit(20);
+            ventZ.setShowTickLabels(true);
+
             shoot.setTranslateX(1700);
             shoot.setTranslateY(1200);
             shoot.setScaleX(2);
             shoot.setScaleY(2);
-            Group controls = new Group(angleX, angleY,  shoot);
+            Group controls = new Group(angleX, angleY, shoot, ventX, ventZ);
             root.getChildren().add(controls);
 
             NumberAxis xAxis = new NumberAxis();
             NumberAxis yAxis = new NumberAxis();
             yAxis.labelProperty().setValue("Height");
             xAxis.labelProperty().setValue("Distance");
-            chart =new LineChart<>(xAxis,yAxis);
+            chart = new LineChart<>(xAxis, yAxis);
             chart.setTranslateX(1820);
             chart.setTranslateY(870);
             root.getChildren().add(chart);
@@ -216,7 +232,7 @@ public class Scenery {
             NumberAxis yAxis2 = new NumberAxis();
             yAxis2.labelProperty().setValue("Distance Gauche/Droite");
             xAxis2.labelProperty().setValue("Distance Profondeur");
-            chart2 =new LineChart<>(xAxis2,yAxis2);
+            chart2 = new LineChart<>(xAxis2, yAxis2);
             chart2.setTranslateX(1820);
             chart2.setTranslateY(300);
             root.getChildren().add(chart2);
@@ -245,5 +261,13 @@ public class Scenery {
 
     public int getPositionCible() {
         return positionCible;
+    }
+
+    public static Slider getVentX() {
+        return ventX;
+    }
+
+    public static Slider getVentZ() {
+        return ventZ;
     }
 }
